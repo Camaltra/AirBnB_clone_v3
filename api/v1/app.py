ed1e2e279e2403374e10ca15a3be0b6e9174f09f
Camaltra <mickael.boillaud@gmail.com>
2022-05-19 09:23:07 +0000

    PycodeStyle Like it

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""
+This is the app file, that manage with the blueprint
+all the route for the API
+"""
 
 from models import storage
 from api.v1.views import app_views
@@ -9,6 +12,7 @@ from flask import Flask
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
@@ -16,4 +20,6 @@ def teardown_db(exception):
 
 
 if __name__ == '__main__':
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=getenv('HBNB_API_PORT', '5000'), threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'),
+            threaded=True)
