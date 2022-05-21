83d210b0a45f969555a46e37fabd906d09389b4e
Camaltra <mickael.boillaud@gmail.com>
2022-05-21 16:17:12 +0000

    Adding the CORS to allowed all connection w/ Colas Renard

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ all the route for the API
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 from flask import Flask, jsonify
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
